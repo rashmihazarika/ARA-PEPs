f0ac30d6a01cb12a1b153ecf6ddb778aace97b3c
R_H <hazarika.rr@gmail.com>
2017-11-20 23:08:46 +0100

    Update SIPs-DB.php

--- SIPs-DB.php
+++ SIPs-DB.php
@@ -43,7 +43,7 @@ if ($sigseq != '') echo ';  only peptides with <b>signal sequence</b> shown';
 echo '</p><p>';
 
 //----------------------------------------------------------------------------------------------
-$servername = "127.0.0.1"; $username = "root"; $password = "AraDB7168#"; $database = "OSIP";
+$servername = "127.0.0.1"; $username = "root"; $password = ""; $database = "OSIP";
 
 $dbhandle = mysql_connect($servername, $username, $password) 
 or die("Unable to connect to MySQL");
